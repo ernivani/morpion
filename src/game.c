8b0f04ad507af3f8c9553e29bb2ec746f8a89447
Thomas Lindeker <tlindeker0@gmail.com>
2024-10-11 11:34:24 +0200

    Etape_5

--- src/game.c
+++ src/game.c
@@ -1,6 +1,5 @@
 #include "game.h"
-#include <stdlib.h>
-#include <time.h>
+#include <limits.h>
 
 Cell board[GRID_SIZE][GRID_SIZE];
 GameState game_state = RUNNING;
@@ -12,7 +11,6 @@ void init_board() {
             board[i][j] = EMPTY;
         }
     }
-    srand(time(NULL));
 }
 
 int check_winner() {
@@ -45,15 +43,68 @@ int check_winner() {
     return 0;
 }
 
-void handle_click(int x, int y) {
-    if (game_state != RUNNING) return;
+int minimax(int depth, int isMaximizing) {
+    int winner = check_winner();
+    
+    if (winner == PLAYER_O) return 10 - depth;
+    if (winner == PLAYER_X) return depth - 10;
+    if (winner == DRAW) return 0;
 
-    int row = y / (600 / GRID_SIZE);
-    int col = x / (600 / GRID_SIZE);
+    if (isMaximizing) {
+        int bestScore = INT_MIN;
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                if (board[i][j] == EMPTY) {
+                    board[i][j] = PLAYER_O;
+                    int score = minimax(depth + 1, 0);
+                    board[i][j] = EMPTY;
+                    bestScore = score > bestScore ? score : bestScore;
+                }
+            }
+        }
+        return bestScore;
+    } else {
+        int bestScore = INT_MAX;
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                if (board[i][j] == EMPTY) {
+                    board[i][j] = PLAYER_X;
+                    int score = minimax(depth + 1, 1);
+                    board[i][j] = EMPTY;
+                    bestScore = score < bestScore ? score : bestScore;
+                }
+            }
+        }
+        return bestScore;
+    }
+}
 
-    if (board[row][col] == EMPTY && current_player == PLAYER_X) {
-        board[row][col] = current_player;
-        current_player = PLAYER_O;
+void computer_play() {
+    if (game_state != RUNNING || current_player != PLAYER_O) return;
+
+    int bestScore = INT_MIN;
+    int moveRow = -1;
+    int moveCol = -1;
+
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            if (board[i][j] == EMPTY) {
+                board[i][j] = PLAYER_O;
+                int score = minimax(0, 0);
+                board[i][j] = EMPTY;
+
+                if (score > bestScore) {
+                    bestScore = score;
+                    moveRow = i;
+                    moveCol = j;
+                }
+            }
+        }
+    }
+
+    if (moveRow != -1 && moveCol != -1) {
+        board[moveRow][moveCol] = PLAYER_O;
+        current_player = PLAYER_X;
 
         int winner = check_winner();
         if (winner == PLAYER_X) {
@@ -63,57 +114,5 @@ void handle_click(int x, int y) {
         } else if (winner == DRAW) {
             game_state = DRAW;
         }
-    }
-}
-
-int find_blocking_move() {
-    for (int i = 0; i < GRID_SIZE; i++) {
-        if (board[i][0] == PLAYER_X && board[i][1] == PLAYER_X && board[i][2] == EMPTY) return i * GRID_SIZE + 2;
-        if (board[i][0] == PLAYER_X && board[i][2] == PLAYER_X && board[i][1] == EMPTY) return i * GRID_SIZE + 1;
-        if (board[i][1] == PLAYER_X && board[i][2] == PLAYER_X && board[i][0] == EMPTY) return i * GRID_SIZE;
-
-        if (board[0][i] == PLAYER_X && board[1][i] == PLAYER_X && board[2][i] == EMPTY) return 2 * GRID_SIZE + i;
-        if (board[0][i] == PLAYER_X && board[2][i] == PLAYER_X && board[1][i] == EMPTY) return GRID_SIZE + i;
-        if (board[1][i] == PLAYER_X && board[2][i] == PLAYER_X && board[0][i] == EMPTY) return i;
-    }
-
-    if (board[0][0] == PLAYER_X && board[1][1] == PLAYER_X && board[2][2] == EMPTY) return 8;
-    if (board[0][0] == PLAYER_X && board[2][2] == PLAYER_X && board[1][1] == EMPTY) return 4;
-    if (board[1][1] == PLAYER_X && board[2][2] == PLAYER_X && board[0][0] == EMPTY) return 0;
-
-    if (board[0][2] == PLAYER_X && board[1][1] == PLAYER_X && board[2][0] == EMPTY) return 6;
-    if (board[0][2] == PLAYER_X && board[2][0] == PLAYER_X && board[1][1] == EMPTY) return 4;
-    if (board[1][1] == PLAYER_X && board[2][0] == PLAYER_X && board[0][2] == EMPTY) return 2;
-
-    return -1;
-}
-
-void computer_play() {
-    if (game_state != RUNNING || current_player != PLAYER_O) return;
-
-    int move = find_blocking_move();
-
-    if (move != -1) {
-        int row = move / GRID_SIZE;
-        int col = move % GRID_SIZE;
-        board[row][col] = PLAYER_O;
-    } else {
-        int row, col;
-        do {
-            row = rand() % GRID_SIZE;
-            col = rand() % GRID_SIZE;
-        } while (board[row][col] != EMPTY);
-        board[row][col] = PLAYER_O;
-    }
-
-    current_player = PLAYER_X;
-
-    int winner = check_winner();
-    if (winner == PLAYER_X) {
-        game_state = X_WINS;
-    } else if (winner == PLAYER_O) {
-        game_state = O_WINS;
-    } else if (winner == DRAW) {
-        game_state = DRAW;
     }
 }
